1b889ee519296fea56aee24096504b97b634cca4
Amy McPherson <qcricket@gmail.com>
2015-11-08 08:54:51 -0800

    Plot 1 working

---
+++ plot1.R
@@ -0,0 +1,21 @@
+## Course Project 1
+## Generate Figure 1
+##
+## Read in data file
+## setwd("~/Documents/data_science/Coursera/04_Exploratory_Data_Analysis/Course Projects")
+df <- read.table("household_power_consumption.txt", 
+                 sep = ";", header = TRUE, na.strings = "?")
+
+## Select only date and Global_active_power columns
+dfFig1 <- select(df, Date, Global_active_power)
+dfFig1 <- filter(dfFig1, Date == "2/1/2007" | Date == "2/2/2007")
+
+## Generate Figure 1
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+hist(dfFig1$Global_active_power, 
+     xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", col = "Red", freq = TRUE, 
+     main = "Global Active Power", 
+     xaxp = c(0,6,3), yaxp = c(0,1200,6), 
+     xpd = NA)
+dev.off()
